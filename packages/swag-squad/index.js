e1b3e17e54a8a5dac5a094b068754711af4ade8e
Ian Walter <pub@ianwalter.dev>
2021-06-07 09:24:09 -0400

    Updating build script

---
+++ packages/swag-squad/index.js
@@ -0,0 +1,12 @@
+export { default as StyledForm } from './components/styled/StyledForm.js'
+export {
+  default as ForgotPasswordForm
+} from './components/forms/ForgotPasswordForm.js'
+export {
+  default as ResetPasswordForm
+} from './components/forms/ResetPasswordForm.js'
+export { default as SignInForm } from './components/forms/SignInForm.js'
+export { default as SignUpForm } from './components/forms/SignUpForm.js'
+export {
+  default as VerifyEmailForm
+} from './components/forms/VerifyEmailForm.js'
