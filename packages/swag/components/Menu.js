03110cd12de15130e06792212041919e572d35a4
Ian Walter <pub@ianwalter.dev>
2021-07-31 22:15:20 -0400

    Menu refactor

--- packages/swag/components/Menu.js
+++ packages/swag/components/Menu.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { merge } from '@generates/merger'
 import { css } from '@stitches/react'
-import {
-  Popover
-  // Transition
-} from '@headlessui/react'
 import { usePopper } from 'react-popper'
 import Button from './buttons/Button.js'
 import StyledDiv from './styled/StyledDiv.js'
@@ -18,62 +14,44 @@ export default function Menu (props) {
     { modifiers: [{ name: 'offset', options: { offset: [0, 10] } }] }
   )
   const popover = css(props.css?.popover)
-  // const noOpacity = css({ opacity: '0' })()
-  // const fullOpacity = css({ opacity: '0' })()
+  const panel = css(merge(
+    {
+      zIndex: 10,
+      overflow: 'hidden',
+      backgroundColor: '#FAFAFA',
+      borderRadius: '.375em',
+      width: '192px',
+      boxShadow: `
+        0 10px 15px -3px rgba(0,0,0,0.1),
+        0 4px 6px -2px rgba(0,0,0,0.05)
+      `
+    },
+    props.css?.panel
+  ))
 
   return (
-    <Popover className={popover()}>
-      {({ open }) => {
-        const panel = css(merge(
-          {
-            zIndex: 10,
-            overflow: 'hidden',
-            backgroundColor: '#FAFAFA',
-            borderRadius: '.375em',
-            width: '192px',
-            boxShadow: `
-              0 10px 15px -3px rgba(0,0,0,0.1),
-              0 4px 6px -2px rgba(0,0,0,0.05)
-            `
-          },
-          props.css?.panel
-        ))
-
-        return (
-          <>
-
-            <Popover.Button
-              as={props.trigger === 'string' ? Button : StyledDiv}
+    <StyledDiv className={popover()}>
+      {typeof props.trigger === 'string'
+        ? (
+            <Button
               ref={setReferenceElement}
               css={merge({ cursor: 'pointer' }, props.css?.button)}
-              {...props.button}
             >
               {props.trigger}
-            </Popover.Button>
+            </Button>
+          )
+        : props.trigger
+      }
 
-            {/* NOTE: Panel transition not working properly. */}
-            {/* <Transition
-              enter={css(transition)().className}
-              enterFrom={noOpacity.className}
-              enterTo={fullOpacity.className}
-              leaveFrom={fullOpacity.className}
-              leaveTo={noOpacity.className}
-            > */}
-              <Popover.Panel
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
-                className={panel()}
-              >
-                {props.children}
-              </Popover.Panel>
-            {/* </Transition> */}
+      <StyledDiv
+        ref={setPopperElement}
+        style={styles.popper}
+        {...attributes.popper}
+        className={panel()}
+      >
+        {props.children}
+      </StyledDiv>
 
-          </>
-        )
-      }}
-    </Popover>
+    </StyledDiv>
   )
 }
-
-Menu.Button = Popover.Button
